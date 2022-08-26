fee19161fb770b3e4060a8ea1eafbd96db5e5867
Paul Zupan <paul.zupan.13@gmail.com>
2022-08-26 10:55:41 -0700

    Add .gitignore

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,4 +1,6 @@
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data")
+  .. "/site/pack/packer/start/packer.nvim"
+
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = vim.fn.system({
     "git",
